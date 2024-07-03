711b11938452003e5940e9e6e0af64fdbf387e5e
SuperSimpleDev <timotheym@live.com>
2024-07-03 19:09:05 +0300

    14-modules delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
     });
 
     document.querySelector('.js-order-summary').innerHTML +=
-    `<div class="">
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -92,6 +92,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId
         removeFromCart(productId);
-        console.log(cart);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
+        container.remove();
     })
 });
