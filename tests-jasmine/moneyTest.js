e891b20fcc49c0b71df8bf2592dfec0fc160ff87
SuperSimpleDev <timotheym@live.com>
2024-07-08 18:37:56 +0300

    16-test jasmine begin

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,21 @@
+import price from '../scripts/utils/price.js';
+
+describe('test suite: price', () => {
+    it('convert cents into dollars', () => {
+        expect(price(2095)).toEqual('20.95');
+    });
+
+    it('work with zero', () => {
+        expect(price(0)).toEqual('0.00');
+    });
+
+    it('round up to the nearest cent', () => {
+        expect(price(2000.5)).toEqual('20.01');
+    });
+
+    it('round down to the nearest cent', () => {
+        expect(price(2000.4)).toEqual('20.00');
+    });
+
+});
+
