f2b0bcdf47409fee039e6cf6b978140985b07224
SuperSimpleDev <timotheym@live.com>
2024-07-11 21:38:13 +0300

    18 backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from './check/orderSummary.js';
 import {renderPaymentSummary} from "./check/paymentSummary.js";
+import '../backend/backend-pactice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
