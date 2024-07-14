1afa5072558f14efb1da21db052df4fa4c3a20e4
SuperSimpleDev <timotheym@live.com>
2024-07-14 10:42:24 +0300

    18 convert promises to async function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,27 @@ import {renderOrderSummary} from './check/orderSummary.js';
 import {renderPaymentSummary} from "./check/paymentSummary.js";
 import {loadProductsFetch} from "./data/products.js";
 
-// здесь мы можем указать promis`ы, которые должны
-// выполнятся одновременно, пока они все не закончатся
+
+// async - преобразует функцию так, чтобы она возвращала promise
+async function loadPage() {
+    // await позволяет писать асинхронный код как обычный
+    // await можно использовать только внутри async function
+    await loadProductsFetch();
+    // чтобы не использовать then мы можем сохранить
+    // результат await в переменную
+
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+
+loadPage().then();
+
+
+
+/* version 2
+  //  здесь мы можем указать promis`ы, которые должны
+  //  выполнятся одновременно, пока они все не закончатся
+
 Promise.all([
     loadProductsFetch()
 
@@ -11,10 +30,13 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+ */
 
 
-/*
-// в js всё делается многопоточно изначально, а так мы выполняем всё последовательно
+/* version 1
+ //  в js всё делается многопоточно изначально,
+ //  а так мы выполняем всё последовательно
+
 new Promise((resolve) => {
     loadProducts(() => {
         resolve('value 1');
