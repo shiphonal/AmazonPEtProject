2449f4aa538ab716adfd39455b3890f0dad00544
SuperSimpleDev <timotheym@live.com>
2024-07-14 10:12:49 +0300

    18 promise async

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,33 @@ import {renderOrderSummary} from './check/orderSummary.js';
 import {renderPaymentSummary} from "./check/paymentSummary.js";
 import {loadProducts} from "./data/products.js";
 
-loadProducts(() => {
+// здесь мы можем указать promis`ы, которые должны
+// выполнятся одновременно, пока они все не закончатся
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value 1');
+        });
+    })
+
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+/*
+// в js всё делается многопоточно изначально, а так мы выполняем всё последовательно
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value 1');
+    });
+
+}).then((value) => {
+    console.log(value);
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+ */
+
+
